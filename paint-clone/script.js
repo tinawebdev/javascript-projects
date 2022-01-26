57ec1864177412766dd54f4aa1b9c51fcf66f601
Kristina <codingkris@yahoo.com>
2022-01-26 18:23:01 +0300

    Paint Clone: canvas setup

---
+++ paint-clone/script.js
@@ -0,0 +1,33 @@
+const activeToolEl = document.getElementById('active-tool');
+const brushColorBtn = document.getElementById('brush-color');
+const brushIcon = document.getElementById('brush');
+const brushSize = document.getElementById('brush-size');
+const brushSlider = document.getElementById('brush-slider');
+const bucketColorBtn = document.getElementById('bucket-color');
+const eraser = document.getElementById('eraser');
+const clearCanvasBtn = document.getElementById('clear-canvas');
+const saveStorageBtn = document.getElementById('save-storage');
+const loadStorageBtn = document.getElementById('load-storage');
+const clearStorageBtn = document.getElementById('clear-storage');
+const downloadBtn = document.getElementById('download');
+const { body } = document;
+
+// Global Variables
+const canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+const context = canvas.getContext('2d');
+let currentSize = 10;
+let bucketColor = '#FFFFFF';
+let currentColor = '#1DABB8';
+
+// Create Canvas
+function createCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - 50;
+  context.fillStyle = bucketColor;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  body.appendChild(canvas);
+}
+
+// On Load
+createCanvas();
